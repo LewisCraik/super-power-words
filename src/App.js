f635df39683c5662b7f9b4e8513ba1f373cb73ff
lewiscraik <mail@lewiscraik.co.uk>
2023-10-17 21:19:59 +0100

    Add basic functionality

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+
+const words = ["I", "No", "Go"];
 
 function App() {
+  const [currentWord, setCurrentWord] = useState("Click to Start");
+
+  function getRandomWord() {
+    const randomIndex = Math.floor(Math.random() * words.length);
+    return words[randomIndex];
+  }
+
+  function handleClick() {
+    const randomWord = getRandomWord();
+    setCurrentWord(randomWord);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p onClick={handleClick}>{currentWord}</p>
       </header>
     </div>
   );
